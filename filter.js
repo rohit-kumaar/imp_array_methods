98bfd38f6c507b1898e0a870d88e92cbdc2b7579
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-05-06 13:06:58 +0530

    Array.from() concept

--- filter.js
+++ filter.js
@@ -15,8 +15,10 @@ console.log(newAges);
 
 // comment filter the object
 // concept The Object.values method returns an array of the object's values.
+// concept If you have an array-like object which you're trying to convert to an array before calling the filter method, use the Array.from method.
 const swiperSlide = document.querySelectorAll(".swiper-slide");
-const values = Object.values(swiperSlide);
+// const values = Object.values(swiperSlide);
+const values = Array.from(swiperSlide);
 
 const newObjDate = values.filter(
   (element) => element.dataset.value > 10000 && element.dataset.value < 25000
