19352ef40a2958b05fba2d155bce5855cedfd75b
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-08-19 21:29:22 +0530

    IIMF async/await

--- async_await.js
+++ async_await.js
@@ -1,13 +1,21 @@
 const URL = "https://jsonplaceholder.typicode.com/users";
 
-const user = async () => {
-  const response = await fetch(URL);
-  const data = await response.json();
-  return data;
-};
+// const user = async () => {
+//   const response = await fetch(URL);
+//   const data = await response.json();
+//   return data;
+// };
 
-const userData = user();
-userData.then((data) => {
-  data.map((element) => console.table(element));
-  //   data.map((element) => console.table(`${element.id} ${element.name}`));
-});
+// const userData = user();
+// userData.then((data) => {
+//   data.map((element) => console.table(element));
+//   //   data.map((element) => console.table(`${element.id} ${element.name}`));
+// });
+
+(async () => {
+  const res = await fetch(URL),
+    data = await res.json();
+  return data;
+})()
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
